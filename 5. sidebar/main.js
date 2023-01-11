0d3f8cc5baa38420fa673434601ee439dd19d9c4
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-01-11 22:21:24 +0900

    🚧 Implement click event of .close-button

--- 5. sidebar/main.js
+++ 5. sidebar/main.js
@@ -1,6 +1,11 @@
 const menubuttonEl = document.querySelector('.menu-button')
 const sideBar = document.querySelector('.sidebar')
+const closeButtonEl = document.querySelector('.close-button')
 
 menubuttonEl.addEventListener('click', function () {
   sideBar.classList.toggle('is-active')
+})
+
+closeButtonEl.addEventListener('click', function () {
+  sideBar.classList.remove('is-active')
 })
